c0dba574c633e9e6898c550ae7f0291be6bde1f3
elliot <abol.agahyi@gmail.com>
2025-12-09 03:10:40 -0700

    implment the metronom based on 120bpm

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,18 @@
 const std = @import("std");
 
-// Windows API Beep function
-extern "kernel32" fn Beep(freq: u32, duration: u32) callconv(.Winapi) c_int;
+// Windows API functions
+extern "user32" fn MessageBeep(uType: u32) callconv(.winapi) i32;
+extern "kernel32" fn Sleep(dwMilliseconds: u32) callconv(.winapi) void;
 
 pub fn main() !void {
     const bpm: u32 = 120;
-    const freq: u32 = 880;
-    const click_ms: u32 = 40;
-
     const beat_ms = 60000 / bpm;
 
+    std.debug.print("Metronome started at {} BPM\n", .{bpm});
+
     while (true) {
-        _ = Beep(freq, click_ms);
-        std.time.sleep(std.time.ns_per_ms * (beat_ms - click_ms));
+        std.debug.print(".", .{});
+        _ = MessageBeep(0);
+        Sleep(beat_ms);
     }
 }
